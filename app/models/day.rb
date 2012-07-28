f3644a2fb2d7ed7da0c5a71228be67466ccec882
Mohamed Azab <3azzab@gmail.com>
2012-07-29 01:54:55 +0200

    fixing issues

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,18 +2,18 @@ class Day
   include Mongoid::Document
   validates_presence_of :day
   validates_uniqueness_of :day   
-  validates_presence_of :fajer
-  validates_presence_of :zuhr
-  validates_presence_of :asr
-  validates_presence_of :maghrib
-  validates_presence_of :isha
-  validates_presence_of :quran
-  validates_presence_of :reading
-  validates_presence_of :sonan
-  validates_presence_of :seyam
-  validates_presence_of :qeyam
-  validates_presence_of :quranQuantity
-  
+  # validates_presence_of :fajer
+  #  validates_presence_of :zuhr
+  #  validates_presence_of :asr
+  #  validates_presence_of :maghrib
+  #  validates_presence_of :isha
+  #  validates_presence_of :quran
+  #  validates_presence_of :reading
+  #  validates_presence_of :sonan
+  #  validates_presence_of :seyam
+  #  validates_presence_of :qeyam
+  #  validates_presence_of :quranQuantity
+  #  
   embedded_in :user
   field :day, :type => Date
   field :fajer, :type => String
@@ -21,10 +21,11 @@ class Day
   field :asr, :type => String
   field :maghrib, :type => String
   field :isha, :type => String
-  field :quran, :type => Boolean
+  #field :quran, :type => Boolean
   field :reading, :type => Boolean
   field :sonan, :type => Integer
   field :seyam, :type => Boolean
   field :qeyam, :type => String
   field :quranQuantity, :type => String
+  field :comments, :type => String
 end
